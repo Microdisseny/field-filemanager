5211f435048f3f0a7b1fe8e050cdb052ef091e89
Alexandre <abusquets@gmail.com>
2018-10-26 15:34:43 +0200

    First version

---
+++ TODO.md
@@ -0,0 +1,4 @@
+# TODO
+
+- Fix use webkitGetAsEntry in old browsers
+- Manage server errors
---
+++ src/components/DropArea.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="drop-area"
+      :class="{'drag-over': isOver}"
+      @dragenter.prevent='onDragEnter'
+      @dragleave.prevent='onDragLeave'
+      @dragover.prevent='onDragOver'
+      @drop.prevent='onDrop'>
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      dragEnterCounter: 0
+    }
+  },
+  computed: {
+    isOver () {
+      return this.dragEnterCounter > 0
+    }
+  },
+  methods: {
+    // https://stackoverflow.com/questions/3590058/does-html5-allow-drag-drop-upload-of-folders-or-a-folder-tree/44918780#44918780
+    getFilesWebkitDataTransferItems (dataTransferItems) {
+      function traverseFileTreePromise (item, path = '') {
+        return new Promise(resolve => {
+          if (item.isFile) {
+            item.file(file => {
+              file.filepath = path + file.name // save full path
+              files.push(file)
+              resolve(file)
+            })
+          } else if (item.isDirectory) {
+            let dirReader = item.createReader()
+            dirReader.readEntries(entries => {
+              let entriesPromises = []
+              for (let entr of entries) {
+                entriesPromises.push(traverseFileTreePromise(entr, path + item.name + '/'))
+              }
+              resolve(Promise.all(entriesPromises))
+            })
+          }
+        })
+      }
+
+      let files = []
+      return new Promise((resolve, reject) => {
+        let entriesPromises = []
+        for (let it of dataTransferItems) {
+          if (it.kind === 'file') {
+            entriesPromises.push(traverseFileTreePromise(it.webkitGetAsEntry()))
+          }
+        }
+        Promise.all(entriesPromises)
+          .then(entries => {
+            resolve(files)
+          })
+      })
+    },
+    onDrop (event) {
+      this.dragEnterCounter = 0
+      const items = event.dataTransfer.items
+      this.getFilesWebkitDataTransferItems(items)
+        .then(files => {
+          this.$emit('newFiles', Array.from(files))
+        })
+    },
+    onDragEnter () {
+      this.dragEnterCounter++
+    },
+    onDragLeave () {
+      this.dragEnterCounter--
+    },
+    onDragOver () {}
+  }
+}
+</script>
+
+<style>
+.drop-area {
+  border: 2px dashed #666;
+  border-radius: 50px;
+}
+.drop-area.drag-over {
+  border: 2px dashed #129d57;
+  background-color: #129d5736;
+  color: #129d57;
+}
+</style>
